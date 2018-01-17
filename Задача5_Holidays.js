4da4f5e65b5f66c161842fe749e1d0cd22d4d904
ymartsevich <34188246+ymartsevich@users.noreply.github.com>
2018-01-17 22:52:43 +0300

    Delete Задача5_Holidays.js

--- Задача5_Holidays.js
+++
@@ -1,8 +0,0 @@
-function car(days){
-    if ( 1 <= days < 7 ) {
-        return ('payment ' + days * 40 + ' dollars,' + ' cash back 20 dollars');
-    } else {
-        return ('payment ' + days * 40 + ' dollars,' + ' cash back 50 dollars')
-    }
-}
-car (7);
