84b5d1051157edfbeba30e235660cb08dea54632
ymartsevich <ymartsevich@gmail.com>
2018-01-19 01:42:14 +0400

    no mistakes

--- Задача5_Holidays.js
+++ Задача5_Holidays.js
@@ -1,8 +1,22 @@
-function car(days){
-    if ( 1 <= days < 7 ) {
-        return ('payment ' + days * 40 + ' dollars,' + ' cash back 20 dollars');
+/*Задача #5 - Адекватная (каникулы)
+
+After a hard quarter in the office you decide to get some rest on a vacation. So you will book a  flight for you and your girlfriend and try to leave all the mess behind you.
+
+    You will need a rental car in order for you to get around in your vacation. The manager of the car rental makes you some good offers.
+
+    Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
+
+    Write a code that gives out the total amount for different days(d).
+
+    The function take one argument - the number of days.*/
+
+
+function car(days) {
+    if (1 <= days < 7) {
+        return ('payment ' + (days * 40 - 20))
     } else {
-        return ('payment ' + days * 40 + ' dollars,' + ' cash back 50 dollars')
+        return ('payment ' + (days * 40 - 50))
     }
 }
-car (7);
+
+console.log(car(7));
